031e69ad2bbb6e5b336249dd0e60e0120fa90f28
parthchavhan <parthjain9460@gmail.com>
2025-12-27 22:05:26 +0530

    hey

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import '@uploadthing/react/styles.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
